ff23f27d923aab7fba0bcbfe5cf612dfa490443c
Mousa Ayoubi <mousaayoubi@gmail.com>
2019-02-08 18:05:11 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,118 +1,98 @@
 // Foursquare API Info
-const clientId = "EXZHFENOPKMNC53M35DICHMKYS1PW4PPJXRQONO4HXZTE5AL";
-const clientSecret = "STOMCBNCTLSRD2DHIW5FEUCNWWQ25Y0BGKNT2HR3KSNJ05XM";
-const url = "https://api.foursquare.com/v2/venues/explore?near=";
+const clientId = 'EXZHFENOPKMNC53M35DICHMKYS1PW4PPJXRQONO4HXZTE5AL';
+const clientSecret = 'STOMCBNCTLSRD2DHIW5FEUCNWWQ25Y0BGKNT2HR3KSNJ05XM';
+const url = 'https://api.foursquare.com/v2/venues/explore?near=';
 
 // APIXU Info
-const apiKey = "7e53027249084f9a8c1104839190702";
-const forecastUrl = "http://api.apixu.com/v1/forecast.json";
+const apiKey = '7e53027249084f9a8c1104839190702';
+const forecastUrl = 'http://api.apixu.com/v1/forecast.json';
 
 // Page Elements
-const $input = $("#city");
-const $submit = $("#button");
-const $destination = $("#destination");
-const $container = $(".container");
+const $input = $('#city');
+const $submit = $('#button');
+const $destination = $('#destination');
+const $container = $('.container');
 const $venueDivs = [$("#venue1"), $("#venue2"), $("#venue3"), $("#venue4")];
-const $weatherDivs = [
-  $("#weather1"),
-  $("#weather2"),
-  $("#weather3"),
-  $("#weather4")
-];
-const weekDays = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday"
-];
+const $weatherDivs = [$("#weather1"), $("#weather2"), $("#weather3"), $("#weather4")];
+const weekDays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
 // Add AJAX functions here:
 const getVenues = async () => {
   const city = $input.val();
-  const urlToFetch =
-    url +
-    city +
-    "&limit=10" +
-    "&client_id=" +
-    clientId +
-    "&client_secret=" +
-    clientSecret +
-    "&v=20190207";
+  const urlToFetch = url + city + '&limit=10' + '&client_id=' + clientId + '&client_secret=' + clientSecret + '&v=20190207';
   try {
     const response = await fetch(urlToFetch);
     if (response.ok) {
-      console.log("API connection is successfull");
+     console.log('API connection is successfull');
       const jsonResponse = await response.json();
       console.log(jsonResponse);
-      const venues = jsonResponse.response.groups[0].items.map(
-        item => item.venue
-      );
+      const venues = jsonResponse.response.groups[0].items.map(item => item.venue);
       console.log(venues);
       return venues;
     } else {
-      throw new Error("Request failed!");
+      throw new Error('Request failed!');
     }
-  } catch (error) {
+  } catch(error) {
     console.log(error.message);
-  }
+  };
 };
 
 const getForecast = async () => {
   const city = $input.val();
-  const urlToFetch =
-    forecastUrl + "?key=" + apiKey + "&q=" + city + "&days=4" + "&hour=10";
+  const urlToFetch = forecastUrl + '?key=' + apiKey + '&q=' + city + '&days=4' + '&hour=10'; 
   try {
     const response = await fetch(urlToFetch);
     if (response.ok) {
-      console.log("API connection is successfull");
+      
+ console.log('API connection is successfull');
       const jsonResponse = await response.json();
       console.log(jsonResponse);
       const days = jsonResponse.forecast.forecastday;
       console.log(days);
       return days;
     } else {
-      throw new Error("Request failed!");
+      throw new Error('Request failed!');
     }
-  } catch (error) {
+  } catch(error) {
     console.log(error.message);
   }
 };
 
+
 // Render functions
-const renderVenues = venues => {
+const renderVenues = (venues) => {
   $venueDivs.forEach(($venue, index) => {
     // Add your code here:
     const venue = venues[index];
-
+  
     const venueIcon = venue.categories[0].icon;
-    console.log(venueIcon);
+    console.log(venueIcon); 
 
-    let venueContent = "";
+    let venueContent = '';
     $venue.append(venueContent);
   });
   $destination.append(`<h2>${venues[0].location.city}</h2>`);
-};
+}
 
-const renderForecast = days => {
+const renderForecast = (days) => {
   $weatherDivs.forEach(($day, index) => {
     // Add your code here:
 
-    let weatherContent = "";
+    
+
+    let weatherContent = '';
     $day.append(weatherContent);
   });
-};
+}
 
 const executeSearch = () => {
   $venueDivs.forEach(venue => venue.empty());
   $weatherDivs.forEach(day => day.empty());
   $destination.empty();
   $container.css("visibility", "visible");
-  getVenues();
-  getForecast();
+  getVenues()
+  getForecast()
   return false;
-};
+}
 
-$submit.click(executeSearch);
+$submit.click(executeSearch)
